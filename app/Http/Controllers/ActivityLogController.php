ef15b9e1f95f3fb6a91444b079b934eb464ebbf1
asephudori <asep.hudori532@gmail.com>
2025-03-21 20:31:07 +0700

    add filter by category for log activity

--- app/Http/Controllers/ActivityLogController.php
+++ app/Http/Controllers/ActivityLogController.php
@@ -11,12 +11,16 @@ class ActivityLogController extends Controller
     {
         $startDate = $request->input('start_date');
         $endDate = $request->input('end_date');
+        $category = $request->input('category');
 
         $logs = ActivityLog::when($startDate, function ($query, $startDate) {
             return $query->where('times', '>=', $startDate);
         })
         ->when($endDate, function ($query, $endDate) {
             return $query->where('times', '<=', $endDate);
+        })
+        ->when($category, function ($query, $category) {
+            return $query->where('category', $category);
         })
         ->with('user')
         ->orderBy('times', 'desc')
